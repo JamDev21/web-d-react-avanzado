ec183101ca5ca2a5ddc8fb0a81e9b7a0669898a9
Jonathan <221150052@atitalaquia.tecnm.mx>
2025-07-17 20:47:28 -0600

    Estilos para el chatBot

--- express/src/app.js
+++ express/src/app.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
