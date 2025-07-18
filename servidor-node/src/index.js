5dda9f7e83ddb2e412a4f2e0560bb8209479de0b
Jonathan <221150052@atitalaquia.tecnm.mx>
2025-07-17 20:08:02 -0600

    servidor Nodejs

---
+++ servidor-node/src/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'contend-Type': 'text/plain' })
+  res.end('Hola mundo, desde Nodejs')
+})
+
+const PORT = 3000
+
+server.listen(PORT, () => {
+    console.log('Servidor ejecutandose en el port http://localhost:3000')
+})
